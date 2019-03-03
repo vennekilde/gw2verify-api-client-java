b990e61c2cfbbf15be20db4001b6c307d30ab499
Jeppe Vennekilde <jeppe@jeppes.info>
2019-03-03 18:34:45 +0100

    Added /users/{service_id}/{service_user_id}/apikey/name endpoint

---
+++ src/main/java/com/farshiverpeaks/gw2verifyclient/model/APIKeyName.java
@@ -0,0 +1,94 @@
+package com.farshiverpeaks.gw2verifyclient.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name"
+})
+public class APIKeyName {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public APIKeyName() {
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public APIKeyName(String name) {
+        super();
+        this.name = name;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public APIKeyName withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public APIKeyName withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("name", name).append("additionalProperties", additionalProperties).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(name).append(additionalProperties).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof APIKeyName) == false) {
+            return false;
+        }
+        APIKeyName rhs = ((APIKeyName) other);
+        return new EqualsBuilder().append(name, rhs.name).append(additionalProperties, rhs.additionalProperties).isEquals();
+    }
+
+}
---
+++ src/main/java/com/farshiverpeaks/gw2verifyclient/resource/users/service_id/service_user_id/apikey/name/model/NamePUTHeader.java
@@ -0,0 +1,21 @@
+
+package com.farshiverpeaks.gw2verifyclient.resource.users.service_id.service_user_id.apikey.name.model;
+
+
+public class NamePUTHeader {
+
+    private String _xAccessToken;
+
+    public NamePUTHeader(String xAccessToken) {
+        _xAccessToken = xAccessToken;
+    }
+
+    public void setXAccessToken(String xAccessToken) {
+        _xAccessToken = xAccessToken;
+    }
+
+    public String getXAccessToken() {
+        return _xAccessToken;
+    }
+
+}
